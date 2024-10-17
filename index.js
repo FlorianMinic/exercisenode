8defc191d9cae9e5a1f4c3ffd47409afdbff4ea6
Florian Minicelle <fminicelle@gmail.com>
2024-10-17 12:02:04 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import cowsay from 'cowsay';
-
-console.log(cowsay.say({
-    text : `My name is ${process.env.DB_NAME}, from ${process.env.DB_CAMPUS}`,
-    e : "oO",
-    T : "U "
-}));
